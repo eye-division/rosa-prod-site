138632f25f01d431e411ebba950e06c7b01e7c99
Alun Ward <a.ward@eyedivision.com>
2024-06-20 15:53:31 +0100

    Update completely for develop branch

--- js/main.js
+++ js/main.js
@@ -13,15 +13,54 @@
         }
       });
     });
-    
+
     // Lightbox2 options
     lightbox.option({
       'wrapAround': true
     });
-    
+
     // fitvid on embed
     $('.media').fitVids();
-    
+
+    $('.homepage-slider').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      dots: true,
+      fade: true,
+      /*vertical: true,
+      verticalSwiping: true,*/
+      cssEase: "linear",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+
+    });
+
   });
 
 })(jQuery); // End of use strict
